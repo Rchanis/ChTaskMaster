06ec922e46973807d808108382ce871d3cf72270
Rchanis <raul.antonio.fernandez1@gmail.com>
2023-08-14 12:16:41 -0600

    redux
    create task reducer
    add constants in actions types
    create a mock up json

---
+++ Front-end/ch-task-master/src/actionsTypes.js
@@ -0,0 +1,6 @@
+export const NEW_TASK = "NEW_TASK";
+export const EDIT_TASK = "EDIT_TASK";
+export const DELETE_TASK = "DELETE_TASK";
+export const DOING_TASK = "DOING_TASK";
+export const DONE_TASK = "DONE_TASK";
+export const TODO_TASK = "TODO_TASK";
