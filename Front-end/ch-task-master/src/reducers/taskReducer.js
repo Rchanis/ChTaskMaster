a37ea2d4f93d8d00214e10550b2bdae564981ef0
Rchanis <raul.antonio.fernandez1@gmail.com>
2023-08-18 14:39:08 -0600

    Resolve state mutation
    implement a deep copy of the state with JSON.parse(JSON.stringify(state)

--- Front-end/ch-task-master/src/reducers/taskReducer.js
+++ Front-end/ch-task-master/src/reducers/taskReducer.js
@@ -22,22 +22,30 @@ const initialState = {
             title: "Consectetur culpa tempor ullamco id.",
             description: "Ullamco nostrud ipsum sunt occaecat. Ex ea cupidatat tempor dolore eiusmod mollit eiusmod commodo laborum pariatur dolore id. Excepteur dolor reprehenderit ipsum esse duis amet consectetur quis exercitation. Laboris irure anim tempor ad culpa proident nisi labore mollit cupidatat. Nulla amet eu est consequat magna minim in quis officia."
         },
+        ],
+        todo: [
+            {
+                id: 0,
+                title: "Consectetur culpa tempor ullamco id.",
+                description: "Ullamco nostrud ipsum sunt occaecat. Ex ea cupidatat tempor dolore eiusmod mollit eiusmod commodo laborum pariatur dolore id. Excepteur dolor reprehenderit ipsum esse duis amet consectetur quis exercitation. Laboris irure anim tempor ad culpa proident nisi labore mollit cupidatat. Nulla amet eu est consequat magna minim in quis officia."
+            }
         ]
     }
-
 
 }
 
 const taskReducer = (state = initialState, action) => {
     switch (action.type) {
         case NEW_TASK:
-            const newState = { ...state };
+            const newState = JSON.parse(JSON.stringify(state));
             const newTask = {
-                id: state.tasks.done[state.tasks.done.length -1].id + 1,
+                id: state.tasks.done[state.tasks.done.length - 1].id + 1,
                 title: action.title,
                 description: action.description
             };
-            newState.tasks.done.push(newTask);
+            
+            newState.tasks.done.push(newTask)
+            
             return newState;
 
         default:
